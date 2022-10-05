1b71f6436ecaf0fc3b3bbc4cfe4def7100b83d7b
wrstdani <ddelnuevo25@gmail.com>
2022-10-05 11:12:01 +0200

    ereased a couple lines

--- first_course/programming/python/p2/P2Ej8.py
+++ first_course/programming/python/p2/P2Ej8.py
@@ -7,7 +7,3 @@ for i in list:
         dinero_restante = euros // i
         print('Hay ' + str(dinero_restante) + (' billete/s' if euros >= 5 else ' moneda/s') + ' de ' + str(i) + ' euros')
         euros = euros % i
-
-
-
-
