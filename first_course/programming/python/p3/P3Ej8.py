8a102fe7adee476bc8be326b8fd85174001a045e
wrstdani <ddelnuevo25@gmail.com>
2022-10-13 10:14:12 +0200

    new exercise (prime numbers)

--- first_course/programming/python/p3/P3Ej8.py
+++ first_course/programming/python/p3/P3Ej8.py
@@ -1,51 +1,47 @@
-n = int(input('Introduzca un valor mayor que 1: '))
-pa = (n - 1)
-pp = (n + 1)
+n = int(input('Introduzca un valor entero: '))
+na = (n - 1)
+np = (n + 1)
 
+if (n <= 1):
+    print('Ha introducido un valor igual o menor a uno, por lo que el programa se cerrará.')
+    quit
 
-def es_primo(n):
-    while n > 1:
+elif (n > 1):
+    def es_primo(n):
         if (n == 2):
             return True
+
         elif (n > 2):
-            for i in range(2, n):
-                if n % i == 0:
+            for i in range (2, n):
+                if (n % i) == 0:
                     return False
-                else:
-                    return True
-
-if es_primo(n) == True:
-    print(str(n) + ' es primo.')
-
-elif es_primo(n) == False:   
-    def es_primo_ant(pa):
-        if (pa == 2):
-            return True
-
-        elif (pa > 2):
-            for i in range(2, pa):
-                if (pa % i) == 0:
-                    while (pa % i) == 0:
-                        pa = pa - 1
                 
                 else:
                     return True
+
+    def es_primo_ant(na):
+        if na == 2:
+            return True
+
+        for i in range (2, na):
+            while na % i == 0:
+                return False
+
+    def es_primo_post(np):
+        for i in range (2, np):
+            while np % i == 0:
+                return False
+
     
-    def es_primo_post(pp):
-        for i in range (2, pp):
-            if (pp % i) == 0:
-                while (pp % i) == 0:
-                    pp = pp + 1
+while es_primo_ant(na) == False:
+    na = na - 1
 
-            else:
-                return True
-        
+while es_primo_post(np) == False:
+    np = np + 1
 
-
-    if (es_primo_ant(pa) == True) and (es_primo_post(pp) == True):
-        print(str(pa) + ' ' + str(pp))
-
-
-elif (n <= 1):
-    print('El valor ha de ser mayor que uno.')
-    exec(open("P3Ej8.py").read())
+if (es_primo(n) == True):
+    print('El número primo inmediatamente anterior a ' + str(n) + ' es ' + str(na) + ' y el inmediatamente posterior es ' + str(np) + '. ')
+    print('Además, el número introducido (' + str(n) + ') es primo.')
+    
+else:
+    print('El número primo inmediatamente anterior a ' + str(n) + ' es ' + str(na) + ' y el inmediatamente posterior es ' + str(np) + '. ')
