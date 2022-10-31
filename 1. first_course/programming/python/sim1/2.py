784fd3a64e027ede92a961be46e391ba6f9637c6
wrstdani <ddelnuevo25@gmail.com>
2022-10-31 17:52:08 +0100

    números ondulados

--- 1. first_course/programming/python/sim1/2.py
+++ 1. first_course/programming/python/sim1/2.py
@@ -1,9 +1,31 @@
-def ond_tres_cifras(num):
-    if (((num % 10) * 100) + ()) == num:
-        return "Es ondulado"
-    else:
-        return "No es ondulado"
+n = int(input())
+n1 = n
+cifras = 0
+a = n % 10
+n = n // 10
+b = n % 10
+n = n // 10
 
+while n1 > 0:
+    cifras += 1
+    n1 = n1 // (10)
 
-num = int(input())
-print(ond_tres_cifras(num))
+caso = True
+ondulado = True
+
+if cifras >= 3:
+    while n > 0:
+        res = n % 10
+        if caso:
+            if res != a:
+                ondulado = False
+        else:
+            if res != b:
+                ondulado = False
+        caso = not caso
+        n = n // 10
+    print(ondulado)
+
+else:
+    print('El número ha de contar con 3 o más cifras.')
+
