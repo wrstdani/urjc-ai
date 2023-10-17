fe48b0b18da180de45b6d6bcc4a6b7519a465060
wrstdani <ddelnuevo25@gmail.com>
2023-10-18 00:44:53 +0200

    data structures examples

---
+++ 2. second_course/edI/10-17-2023/listas.c
@@ -0,0 +1,5 @@
+//
+// Created by Daniel del Nuevo Montero on 18/10/23.
+//
+
+#include "listas.h"
---
+++ 2. second_course/edI/10-17-2023/listas.h
@@ -0,0 +1,8 @@
+//
+// Created by Daniel del Nuevo Montero on 18/10/23.
+//
+
+#ifndef INC_10_17_2023_LISTAS_H
+#define INC_10_17_2023_LISTAS_H
+
+#endif //INC_10_17_2023_LISTAS_H
