a304b6a9233ac5b7e58cd62701ba4d13e1c8d414
wrstdani <ddelnuevo25@gmail.com>
2023-10-18 15:19:57 +0200

    update

--- 2. second_course/edI/10-17-2023/listas.c
+++ 2. second_course/edI/10-17-2023/listas.c
@@ -3,3 +3,32 @@
 //
 
 #include "listas.h"
+
+void mostrarElemento(tElemento info) {
+    printf("%s: %d\n", info.name, info.age);
+}
+
+void imprimirLista(tLista lista) {
+    int pos = lista.primOcupada;
+    while (pos != -1) {
+        mostrarElemento(lista.almacen[pos].info);
+        pos = lista.almacen[pos].sig;
+    }
+}
+
+void construir(tLista *lista, tElemento e) {
+    asignarElemento(&(lista -> almacen[lista -> primLibre].info), e);
+    int aux = lista -> almacen[lista -> primLibre].sig;
+    lista -> almacen[lista -> primLibre].sig = lista -> primOcupada;
+    lista -> primOcupada = lista -> primLibre;
+    lista -> primLibre = aux;
+}
+
+void crearVacia(tLista *lista, int max) {
+    lista -> primLibre = 0;
+    lista -> primOcupada = -1;
+    for (int i = 0; i < max-1; ++i) {
+        lista -> almacen[i].sig = i+1;
+    }
+    lista -> almacen[max-1].sig = -1;
+}
--- 2. second_course/edI/10-17-2023/listas.h
+++ 2. second_course/edI/10-17-2023/listas.h
@@ -5,4 +5,28 @@
 #ifndef INC_10_17_2023_LISTAS_H
 #define INC_10_17_2023_LISTAS_H
 
+#include <stdio.h>
+
+typedef struct {
+    char name[20];
+    int age;
+} tElemento;
+
+typedef struct {
+    tElemento info;
+    int sig;
+} tNodo;
+
+typedef struct {
+    tNodo almacen[100];
+    int primOcupada;
+    int primLibre;
+} tLista;
+
+void mostrarElemento(tElemento info);
+void imprimirLista(tLista lista);
+void asignarElemento(tElemento info, tElemento e);
+void construir(tLista *lista, tElemento e);
+void crearVacia(tLista *lista, int max);
+
 #endif //INC_10_17_2023_LISTAS_H
